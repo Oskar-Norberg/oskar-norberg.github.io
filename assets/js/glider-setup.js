e06c73f9cab93efc10d39c02695d06af76a2c910
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-28 15:57:08 +0200

    EDIT: [Script] Different glider settings per device

--- assets/js/glider-setup.js
+++ assets/js/glider-setup.js
@@ -1,11 +1,42 @@
+const MOBILE_PER_VIEW = 2
+const DESKTOP_PER_VIEW = 3
+const MOBILE_SIZE_BREAKPOINT = 800
+
 document.addEventListener("DOMContentLoaded", function () {
     const glideElements = document.querySelectorAll('.glide');
+    const glides = [];
 
-    glideElements.forEach((el) => {
-        new Glide(el, {
+    function createGlide(el, perView) {
+        return new Glide(el, {
             type: 'carousel',
-            perView: 3,
+            perView: perView,
             focusAt: 'center'
-        }).mount();
-    });
+        });
+    }
+
+    function updateGlides() {
+        const isMobile = window.innerWidth <= MOBILE_SIZE_BREAKPOINT;
+        const perView = isMobile ? MOBILE_PER_VIEW : DESKTOP_PER_VIEW;
+
+        glideElements.forEach((el, index) => {
+            const currentGlide = glides[index];
+
+            if (currentGlide && currentGlide.settings.perView !== perView) {
+                currentGlide.destroy();
+                const newGlide = createGlide(el, perView);
+                newGlide.mount();
+                glides[index] = newGlide;
+            }
+
+            if (!currentGlide) {
+                const newGlide = createGlide(el, perView);
+                newGlide.mount();
+                glides[index] = newGlide;
+            }
+        });
+    }
+
+    updateGlides();
+
+    window.addEventListener('resize', updateGlides);
 });
