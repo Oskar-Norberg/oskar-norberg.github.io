4c424dd860a8a3ee904bb0c1197271a68e1cfd90
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-12-08 15:29:25 +0100

    ADD: [Script] scroll-button javascript
    ADD: [Script] scroll-button javascript

---
+++ assets/js/scroll-button-setup.js
@@ -0,0 +1,43 @@
+const attributeName = "data-scroll-to";
+
+const scrollToTopID = "home";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollButtons = document.querySelectorAll(`[${attributeName}]`);
+
+    scrollButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const targetID = button.getAttribute(attributeName);
+            const target = document.getElementById(targetID);
+
+            if (!target) {
+                if (targetID === scrollToTopID) {
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                }
+
+                return;
+            }
+
+            event.preventDefault();
+
+            target.scrollIntoView({ behavior: "smooth", block: "start" });
+
+            history.replaceState(null, "", `#${targetID}`);
+        });
+    });
+});
+
+window.addEventListener("load", () => {
+    const hash = window.location.hash;
+
+    if (!hash) {
+        return;
+    }
+
+    const target = document.querySelector(hash);
+    if (!target) {
+        return;
+    }
+
+    target.scrollIntoView({ behavior: "smooth", block: "start" });
+});
